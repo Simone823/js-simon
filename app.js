a31bcb3c952317dd8a5ea8dd3c389bcb53d00457
Simone823 <simone.daglio823@outlook.it>
2022-02-22 16:47:09 +0100

    alert, array numeriUtente, setTimeout numUtente

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+// Visualizzare in pagina 5 numeri casuali. ( con un alert )
+// Dopo la chiusura dell’alert parte un timer di 30 secondi.
+// Alla fine dei 30 secondi l’utente dovrà inserire, uno alla volta, i numeri che ha visto precedentemente ( se li ricorda ), tramite il prompt() (servirà un ciclo…?).
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+// NB. l ’ordine dei numeri inseriti non importa, basta che siano presenti nell’elenco di numeri iniziale.
+
+
 // Genero un numero random 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
-console.log(getRandomInt(1, 100));
+// console.log(getRandomInt(1, 100));
 
 // Array numeri random
 const numeriRandom = [];
-console.log(numeriRandom);
+// console.log(numeriRandom);
 
 // Finche la lunghezza dell'array numeriRandom non è 5, pusho la funzione per generare un numero random
 while (numeriRandom.length < 5){
@@ -19,3 +26,21 @@ while (numeriRandom.length < 5){
     }
 }
 console.log(numeriRandom);
+
+
+// Messaggio alert numeriRandom
+alert(numeriRandom);
+
+// Array contenente i numeri digitati dall'utente
+const numeriUtente = [];
+console.log(numeriUtente);
+
+// Dopo 30 secondi chiedo all'utente di inserire i numeri visualizzati dal primo alert
+setTimeout ( function() {
+
+    while (numeriUtente.length < 5){
+        let numUtente = parseInt(prompt("Inserisci un numero visualizzato 30 secondi fa!"));
+        numeriUtente.push(numUtente);
+    }
+
+}, 30000)
